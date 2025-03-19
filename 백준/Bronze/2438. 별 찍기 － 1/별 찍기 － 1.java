b043d69818b09ff692bc5d04c406811698f5b18b
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2025-03-19 20:06:01 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 180 ms, Memory: 15024 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
