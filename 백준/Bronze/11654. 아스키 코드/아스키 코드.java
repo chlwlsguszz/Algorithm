39f090ee2ace1e8171f138f54078f703ef9960bc
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2025-03-19 20:21:29 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        String s = br.readLine();
+        int ascii = s.charAt(0);
+        System.out.println(ascii);
+    }
+}
+
