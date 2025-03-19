3fdcce4ae8a75086bff277eab149e3da1bce02c1
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2025-03-19 20:15:31 +0900

    [Bronze V] Title: A+B - 3, Time: 108 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            System.out.println(a + b);
+        }
+    }
+}
+
