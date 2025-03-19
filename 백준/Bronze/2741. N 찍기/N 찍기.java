9c52fb377339766a3ec9881f3d33a891bdf0b0c3
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2025-03-19 20:10:32 +0900

    [Bronze V] Title: N 찍기, Time: 740 ms, Memory: 29916 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
