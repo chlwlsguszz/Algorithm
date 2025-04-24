aa8abe81bb62d0f815a574381ca5163c7b215fc8
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2025-04-24 20:09:03 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -14,11 +14,7 @@ ON
 WHERE
     ri.ADDRESS LIKE '서울%'
 GROUP BY
-    ri.REST_ID,
-    ri.REST_NAME,
-    ri.FOOD_TYPE,
-    ri.FAVORITES,
-    ri.ADDRESS
+    ri.REST_ID
 ORDER BY
     SCORE DESC,
     ri.FAVORITES DESC;
