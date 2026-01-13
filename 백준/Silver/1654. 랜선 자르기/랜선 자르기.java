473fb530d3f3be5c406b1e82bed2f66096ecba28
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-01-13 14:58:01 +0900

    [Silver II] Title: 랜선 자르기, Time: 160 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,45 +1,55 @@
-import java.io.*;
-import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.*;
 
 public class Main {
+
+    static int[] arr;
+    static int K;
+    static int N;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int K = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int[] arr = new int[K];
-        long min = 0;
-        long mid = 0;
-        long max = 0;
+        st = new StringTokenizer(br.readLine());
+        K = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i<K; i++) {
+        arr = new int[K];
+        long left=1;
+        long right=Integer.MIN_VALUE;
+
+        for(int i=0;i<K;i++) {
             arr[i] = Integer.parseInt(br.readLine());
-            if (max<arr[i])
-                max = arr[i];
+            right = Math.max(right, arr[i]);
         }
 
-        max++;
-        long count;
-        while (min < max) {
-            mid = (max + min) / 2;
-            //bw.write(mid+"\n");
-            count = 0;
-            for (int j : arr) {
-                count += (j / mid);
+        //System.out.println("left = " + left + " and right = " + right);
+        long result = -1;
+        while(left <= right) {
+            long mid = (left+right)/2;
+            //System.out.println("mid = "+ mid);
+            if (can(mid)) {
+                left = mid + 1;
+                result = mid;
             }
-
-            if(count < N) {
-                max = mid;
-            }
-            else {
-                min = mid + 1;
-            }
+            else right = mid - 1;
+            //System.out.println("left = " + left + " and right = " + right);
         }
-        min--;
-        bw.write(min+"\n");
-        bw.flush();
+
+        System.out.println(result);
+        if(result == -1) System.out.println("Failed");
+
+    }
+
+    static boolean can(long mid) {
+        int sum = 0;
+        for(int i=0;i<K;i++) {
+            sum += arr[i]/mid;
+        }
+
+        return sum >= N;
     }
 }
