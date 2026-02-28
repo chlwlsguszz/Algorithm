178e1f656c753af12ad1979d446124e9dbb01d83
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-02-28 18:42:26 +0900

    [Silver V] Title: 단어 정렬, Time: 372 ms, Memory: 24676 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String arr[] = new String[n];
+        for(int i=0;i<n;i++) {
+            arr[i] = br.readLine();
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length() == o2.length())
+                    return o1.compareTo(o2);
+                else
+                    return o1.length() - o2.length();
+            }
+        });
+
+        bw.write(arr[0]+"\n");
+        for(int i=1;i<n;i++) {
+            if(!arr[i-1].equals(arr[i]))
+                bw.write(arr[i]+"\n");
+        }
+        bw.flush();
+    }
+}
