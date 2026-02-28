89d12275543d5faf36ada72d29f0dfdc0c18440c
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-02-28 18:16:47 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 96 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,28 +1,32 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-
-
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        System.out.println(gcd(A,B));
-        System.out.println(lcm(A,B));
+        st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(gcd(a,b));
+        System.out.println(lcm(a,b));
+
     }
 
-    public static int gcd(int a, int b) {
-        if (b==0) return a;
+    static int gcd(int a, int b) {
+        if(b==0)
+            return a;
         return gcd(b, a%b);
     }
 
-    public static int lcm(int a, int b) {
-        return a * b / gcd(a, b);
+    static int lcm(int a, int b) {
+        return a / gcd(a,b) * b;
     }
-
 }
+
 
