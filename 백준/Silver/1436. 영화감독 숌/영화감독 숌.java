7ba10d0045ab6e11c805cae54764a6c559d8498c
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-03-01 18:16:18 +0900

    [Silver V] Title: 영화감독 숌, Time: 252 ms, Memory: 84232 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -2,25 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-
-
     public static void main(String[] args) throws IOException {
-        int N = Integer.parseInt(br.readLine());
-        int i=0;
-        int count=0;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int i = 0;
+        int count = 0;
         while(true) {
-            if (Integer.toString(i).contains("666"))
+            i++;
+            if(Integer.toString(i).contains("666"))
                 count++;
 
-            if(count == N) {
-                System.out.println(i);
-                break;
-            }
-            i++;
+            if (count == n) break;
         }
-    }
 
+        System.out.println(i);
+    }
 }
+
 
