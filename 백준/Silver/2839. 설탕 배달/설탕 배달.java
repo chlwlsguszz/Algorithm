d6ab4de3ba7dcaae18a3210a6a1b37e4c82c8d96
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-03-03 15:12:43 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int bigBag = 5; // 5kg
+        int smallBag = 3; // 3kg
+        int i = N/bigBag;
+        while(i >= 0) {
+            int restSugar = N;
+            if (i!=0) restSugar -= (bigBag*i);
+            if (restSugar % smallBag == 0) {
+                System.out.println(i + (restSugar/smallBag));
+                return;
+            }
+            i--;
+        }
+        System.out.println(-1);
+    }
+}
+
+
