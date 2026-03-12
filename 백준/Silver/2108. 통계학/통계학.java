52661fadb08dd5f509681478dcca8249d819354f
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-03-12 20:42:00 +0900

    [Silver II] Title: 통계학, Time: 676 ms, Memory: 66828 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] array = new int[n];
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+
+        int sum = 0;
+        int max = -4000;
+        int min = 4000;
+
+        for (int i=0;i<n;i++) {
+            int x = Integer.parseInt(br.readLine());
+            sum += x;
+            min = Math.min(min,x);
+            max = Math.max(max,x);
+            array[i] = x;
+
+            freqMap.put(x, freqMap.getOrDefault(x, 0) + 1);
+        }
+
+        Arrays.sort(array);
+
+        int middleValue;
+        if(n%2==0)
+            middleValue = (array[n/2] + array[(n/2)-1])/2;
+        else
+            middleValue = array[n/2];
+
+
+
+        int maxFreq = 0;
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i : freqMap.keySet()) {
+            if(maxFreq <= freqMap.get(i)) {
+                if(maxFreq == freqMap.get(i))
+                    result.add(i);
+                else {
+                    maxFreq = freqMap.get(i);
+                    result.clear();
+                    result.add(i);
+                }
+            }
+        }
+
+        System.out.println(Math.round((float)sum/n));
+
+        System.out.println(middleValue);
+
+        if(result.size() > 1) {
+            Collections.sort(result);
+            System.out.println(result.get(1));
+        }
+        else
+            System.out.println(result.get(0));
+
+        System.out.println(max-min);
+
+    }
+}
+
+
