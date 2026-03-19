afd16b68156e293fa10110905b53a31efe460ee2
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-03-19 17:20:48 +0900

    [Silver III] Title: 파도반 수열, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        long[] dp = new long[101];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 1;
+        dp[4] = 2;
+        dp[5] = 2;
+        dp[6] = 3;
+        dp[7] = 4;
+        dp[8] = 5;
+        dp[9] = 7;
+        dp[10] = 9;
+
+        for(int i=11;i<=100;i++) {
+            dp[i] = dp[i-1] + dp[i-5];
+        }
+
+        while (T-->0) {
+            int N = Integer.parseInt(br.readLine());
+            System.out.println(dp[N]);
+        }
+
+    }
+}
