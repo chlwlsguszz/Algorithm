5fc7fadadb80d1584fce4ba62bef1e3137317c2c
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-03-19 16:09:41 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -2,22 +2,22 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n = 0;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
 
-        while(T-- > 0) {
-            n = Integer.parseInt(br.readLine());
-            int result = find(0);
-            System.out.println(result);
+        int[] dp = new int[12];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for(int i=4;i<=11;i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
         }
-    }
 
-    static int find(int x) {
-        if(x == n) return 1;
-        else if (x > n) return 0;
-        return find(x+1) + find(x+2) + find(x+3);
+        while(T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            System.out.println(dp[n]);
+        }
     }
 }
