5d143227c9a30c653ba8c4cecb0f818d1216da96
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-03-19 17:00:32 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        StringTokenizer st;
+
+        while (T-->0) {
+            int n = Integer.parseInt(br.readLine());
+
+            for(int i=0;i<n;i++) {
+                st = new StringTokenizer(br.readLine());
+                st.nextToken();
+                String s = st.nextToken();
+                hashMap.put(s, hashMap.getOrDefault(s,0) + 1);
+            }
+            ArrayList<Integer> values = new ArrayList<>(hashMap.values());
+            if(values.isEmpty())
+                System.out.println(0);
+            else if(values.size() == 1)
+                System.out.println(values.get(0));
+            else {
+                int result = 1;
+                for(int x : values) {
+                    result *= x+1;
+                }
+                System.out.println(result-1);
+            }
+            hashMap.clear();
+        }
+
+    }
+}
