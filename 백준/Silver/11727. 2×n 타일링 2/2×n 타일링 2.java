c27958cec4c9e54db92f76b8e7f3d643f9d9beb9
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-03-20 15:08:30 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for(int i=3;i<=n;i++) {
+            dp[i] = (dp[i-2]*2 + dp[i-1]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
