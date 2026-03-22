0b9a9c25661e2066c48b7c0b3b53f50791c803fe
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-03-22 16:35:35 +0900

    [Silver II] Title: 랜선 자르기, Time: 176 ms, Memory: 17444 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,55 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    static int[] arr;
-    static int K;
-    static int N;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new java.io.InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine());
-        K = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-
-        arr = new int[K];
-        long left=1;
-        long right=Integer.MIN_VALUE;
+        long[] cables = new long[K];
 
         for(int i=0;i<K;i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-            right = Math.max(right, arr[i]);
+            cables[i] = Integer.parseInt(br.readLine());
         }
 
-        //System.out.println("left = " + left + " and right = " + right);
-        long result = -1;
+        Arrays.sort(cables);
+
+        long left = 1;
+        long right = cables[K-1];
+        long mid = 1;
+        long result = 1;
         while(left <= right) {
-            long mid = (left+right)/2;
-            //System.out.println("mid = "+ mid);
-            if (can(mid)) {
-                left = mid + 1;
+            mid = (left + right)/2;
+            long sum = 0;
+            for(int i=0;i<K;i++) {
+                sum += cables[i]/mid;
+            }
+            if (sum>=N) {
                 result = mid;
+                left = mid + 1;
             }
             else right = mid - 1;
-            //System.out.println("left = " + left + " and right = " + right);
         }
 
         System.out.println(result);
-        if(result == -1) System.out.println("Failed");
 
     }
 
-    static boolean can(long mid) {
-        int sum = 0;
-        for(int i=0;i<K;i++) {
-            sum += arr[i]/mid;
-        }
-
-        return sum >= N;
-    }
 }
