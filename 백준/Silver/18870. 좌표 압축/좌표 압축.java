bf2cad097ae9ccc3512ec6946c3b76e132300ba1
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-03-24 17:20:16 +0900

    [Silver II] Title: 좌표 압축, Time: 2000 ms, Memory: 276828 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -2,42 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
 
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
         int N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        int[] points = new int[N];
+        int[] array = new int[N];
+        TreeSet<Integer> treeSet = new TreeSet<>();
 
-        for(int i = 0; i < N; i++) {
-            points[i] = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++) {
+            int x = Integer.parseInt(st.nextToken());
+            array[i] = x;
+            treeSet.add(x);
         }
 
-        int[] sortedPoints = points.clone();
-        Arrays.sort(sortedPoints);
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(sortedPoints[0], 0);
+        int count=0;
+        for(int x : treeSet)
+            hashMap.put(x,count++);
 
-        int count = 0;
-        for(int i=1;i<N;i++) {
-            if(sortedPoints[i-1] < sortedPoints[i]) {
-                count++;
-                map.put(sortedPoints[i], count);
-            }
-        }
+
 
         StringBuilder sb = new StringBuilder();
-        for(int num: points) {
-            sb.append(map.get(num)).append(" ");
+        for(int i=0;i<N;i++) {
+            sb.append(hashMap.get(array[i])).append(" ");
         }
 
         System.out.println(sb);
-        //for(int i=0; i<N; i++) {
-        //    System.out.print(map.get(points[i])+" ");
-        //}
-    }
-}
 
+
+
+    }
+
+}
