8f16ab71e3dcc9b990c1eef176bb91850477c14a
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-03-26 12:25:41 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 112 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -2,53 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int sum = 0;
-    static boolean flag = false;
-    static ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
+        ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
+        int sum = 0;
+        boolean flag = false;
+
+        int num = 0;
 
         for(int i=0;i<s.length();i++) {
             char c = s.charAt(i);
 
-            if(c == '-') {
-                calcSum();
-                flag = true;
-            }
+            if(c == '+' || c == '-') {
+                if (!flag)
+                    sum += num;
+                else
+                    sum -= num;
 
-            else if (c == '+') {
-                calcSum();
+                if(c=='-')
+                    flag = true;
+                
+                num = 0;
             }
-
 
             else {
-                queue.add(Character.getNumericValue(c));
+                num = num * 10 + (c-'0');
             }
 
         }
 
-        calcSum();
+        if (!flag)
+            sum += num;
+        else
+            sum -= num;
 
         System.out.println(sum);
 
     }
-
-    static void calcSum() {
-        int x = 0;
-        while(!queue.isEmpty()) {
-            int multiplier = 1;
-            for(int j=1;j<queue.size();j++) {
-                multiplier *= 10;
-            }
-            x += queue.poll()*multiplier;
-        }
-
-        if(!flag)
-            sum += x;
-        else
-            sum -= x;
-    }
-
 }
