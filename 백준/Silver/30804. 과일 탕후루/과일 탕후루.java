126fc3f8e7d161521713b73dfca8ae0aa0aba5b5
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-03-30 17:56:53 +0900

    [Silver II] Title: 과일 탕후루, Time: 420 ms, Memory: 35276 KB -BaekjoonHub

--- 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
+++ 백준/Silver/30804. 과일 탕후루/과일 탕후루.java
@@ -2,55 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] count = new int[10];
-    static int[] arr = new int[200000];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for(int i=0;i<N;i++)
             arr[i] = Integer.parseInt(st.nextToken());
 
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
 
         int left = 0;
         int right = 0;
         int max = 0;
 
         while(right < N) {
-            while(right < N) {
-                count[arr[right]]++;
-                if(checkSize()>2) {
-                    count[arr[right]]--;
-                    break;
-                }
-                right++;
-            }
-            //총 개수 계산
-            max = Math.max(max,right-left);
+            hashMap.put(arr[right],hashMap.getOrDefault(arr[right],0)+1);
+            right++;
 
-            while(left <= right) {
-                count[arr[left]]--;
+            while(hashMap.size() > 2) {
+                hashMap.put(arr[left], hashMap.get(arr[left])-1);
+                if(hashMap.get(arr[left]) == 0)
+                    hashMap.remove(arr[left]);
                 left++;
-                if(checkSize()<2) {
-                    break;
-                }
             }
+
+            max = Math.max(max,right-left);
         }
 
         System.out.println(max);
 
-    }
-
-    static int checkSize() {
-        int groups = 0;
-        for(int i=1;i<=9;i++) {
-            if(count[i] > 0)
-                groups++;
-        }
-        return groups;
     }
 
 
