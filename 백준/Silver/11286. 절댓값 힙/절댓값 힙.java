2ad996894174eeccdc5dd7a76b48ae22a086116a
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-04-07 15:19:46 +0900

    [Silver I] Title: 절댓값 힙, Time: 560 ms, Memory: 28648 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -2,30 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static PriorityQueue<Integer> minHeap = new PriorityQueue<>(new Comparator<Integer>() {
-        @Override
-        public int compare(Integer o1, Integer o2) {
-            if(Math.abs(o2) == Math.abs(o1))
-                return Integer.compare(o1, o2);
-            else
-                return Integer.compare(Math.abs(o1), Math.abs(o2));
-        }
-    });
 
     public static void main(String[] args) throws IOException {
-       int N = Integer.parseInt(br.readLine());
-       for(int i=0;i<N;i++) {
-           int x = Integer.parseInt(br.readLine());
-           if (x==0) {
-               if(minHeap.isEmpty())
-                   System.out.println(0);
-               else
-                   System.out.println(minHeap.poll());
-           }
-           else
-               minHeap.add(x);
-       }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> absMinHeap = new PriorityQueue<>( (a, b) -> {
+                if(Math.abs(a) == Math.abs(b))
+                    return a-b;
+                else
+                    return Math.abs(a) - Math.abs(b);
+            }
+        );
+
+
+        for(int i=0;i<N;i++) {
+            int x = Integer.parseInt(br.readLine());
+            if(x==0) {
+                if (absMinHeap.isEmpty())
+                    System.out.println(0);
+                else {
+                    System.out.println(absMinHeap.poll());
+                }
+            }
+            else
+                absMinHeap.add(x);
+        }
+
     }
+
+
+
 }
