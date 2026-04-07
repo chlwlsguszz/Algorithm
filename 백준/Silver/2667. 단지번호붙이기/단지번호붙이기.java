fcdda69364302c634260a7065c0280c500610fbf
Choi Jin Hyun <82369632+chlwlsguszz@users.noreply.github.com>
2026-04-07 14:52:19 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,72 +1,60 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-
-
-    static int visited[][];
-    static int house[][];
     static int N;
-    static int k;
+    static int[][] map;
+    static boolean[][] visited;
+    static int count = 0;
+    static int[][] dir = { {0,1}, {0,-1}, {1,0}, {-1,0} };
+
 
     public static void main(String[] args) throws IOException {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        visited = new int[N][N];
-        house = new int[N][N];
+
+        map = new int[N][N];
+        visited = new boolean[N][N];
+        ArrayList<Integer> countList = new ArrayList<>();
 
         for(int i=0;i<N;i++) {
             String s = br.readLine();
             for(int j=0;j<N;j++) {
-                house[i][j] = Integer.parseInt(s.charAt(j)+"");
+                map[i][j] = s.charAt(j) - '0';
             }
         }
-
-        for(int a[]: visited)
-            Arrays.fill(a,0);
-
-        int cnt = 0;
-        ArrayList<Integer> arrList = new ArrayList<Integer>();
 
         for(int i=0;i<N;i++) {
             for(int j=0;j<N;j++) {
-                if(house[i][j]==1 && visited[i][j]==0) {
-                    cnt++;
-                    k=0;
-                    dfs(i,j);
-                    arrList.add(k);
+                if(map[i][j] == 1 && !visited[i][j]) {
+                    dfs(i, j);
+                    countList.add(count);
+                    count = 0;
                 }
-                else
-                    visited[i][j] = 1;
             }
         }
 
-        System.out.println(cnt);
-        arrList.sort(Integer::compareTo);
-        for(int a : arrList) {
-            System.out.print(a+" ");
+        Collections.sort(countList);
+        System.out.println(countList.size());
+        for(int x : countList) {
+            System.out.println(x);
+        }
+
+
+    }
+
+    static void dfs(int y, int x) {
+        visited[y][x] = true;
+        count++;
+
+        for(int i=0;i<4;i++) {
+            int ny = y + dir[i][0];
+            int nx = x + dir[i][1];
+            if(ny >= 0 && nx >= 0 && ny < N && nx < N && map[ny][nx] == 1 && !visited[ny][nx]) {
+                dfs(ny, nx);
+            }
         }
     }
 
-    static void dfs(int i, int j) {
-        visited[i][j] = 1;
-        k++;
-        if(j+1 < N && house[i][j+1] == 1 && visited[i][j+1] == 0) {
-            dfs(i,j+1);
-        }
-        if(j-1 > -1 && house[i][j-1] == 1 && visited[i][j-1] == 0) {
-            dfs(i,j-1);
-        }
-        if(i+1 < N && house[i+1][j] == 1 && visited[i+1][j] == 0) {
-            dfs(i+1,j);
-        }
-        if(i-1 > -1 && house[i-1][j] == 1 && visited[i-1][j] == 0) {
-            dfs(i-1,j);
-        }
-    }
 
 }
